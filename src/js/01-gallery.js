b71680024467c98edd57ba3c3be3070c2a4c35ac
Borrouz <brovkov.ievgen@gmail.com>
2023-09-10 00:09:05 +0300

    first task(2)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,16 +1,7 @@
 'use strict'
 // todo 
-// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-
-// 2.Реализация делегирования на ul.gallery и получение url большого изображения.
-
-// 3.Подключение скрипта и стилей библиотеки модального окна basicLightbox.
-//  Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-
-//  4.Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-
-//  5.Замена значения атрибута src элемента <img> в модальном окне перед открытием. 
-//  Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+// 1. Чтоб не качались картинки
+// 2. чтоб esc закрывал картинку
 
 
 
@@ -33,7 +24,6 @@ gallery.insertAdjacentHTML('beforeend', galleryLi);
 gallery.addEventListener("click", onClick);
 
 function onClick (e) {
-    console.log(e.target.alt);
     const dataPreview = e.target.dataset.source;
     const imgDesc = e.target.alt;
     const instance = basicLightbox.create(`
